d92919833eee8476a8f22d0d8cb56a343daa58fa
DeCaelo <ludovic.cleuet@gmail.com>
2023-12-05 21:38:30 +0100

    add theme & tailwind

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,21 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import ComboSelectController from "./combo_select_controller.js";
+application.register("combo-select", ComboSelectController);
+
+import DropdownController from "./dropdown_controller.js";
+application.register("dropdown", DropdownController);
+
+import ModalController from "./modal_controller.js";
+application.register("modal", ModalController);
+
+import TabController from "./tabs_controller.js";
+application.register("tabs", TabController);
+
+import NavController from "./nav_controller.js";
+application.register("nav", NavController);
+
+import TooltipController from "./tooltip_controller.js";
+application.register("tooltip", TooltipController);
